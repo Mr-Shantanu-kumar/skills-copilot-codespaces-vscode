14af92c78d767a2966b4ee6c7734a24d60fd5a19
Shantanu Kumar <54388980+Mr-Shantanu-kumar@users.noreply.github.com>
2023-07-23 14:50:39 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,87 +1,13 @@
 //create web server
-const express = require('express');
-const router = express.Router();
-const db = require('../models');
-const { Comment } = require('../models');
+var express = require('express');
+var router = express.Router();
 
-// GET /comments
-router.get('/', (req, res) => {
-    db.Comment.findAll()
-    .then(comments => {
-        res.json(comments);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).end();
+//get all comments
+router.get('/', function(req, res) {
+        console.log("get all comments");
     });
-});
 
-// GET /comments/:id
-router.get('/:id', (req, res) => {
-    db.Comment.findOne({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(comment => {
-        res.json(comment);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).end();
-    });
-});
 
-// POST /comments
-router.post('/', (req, res) => {
-    db.Comment.create({
-        name: req.body.name,
-        email: req.body.email,
-        message: req.body.message
-    })
-    .then(newComment => {
-        res.json(newComment);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).end();
-    });
-});
 
-// PUT /comments/:id
-router.put('/:id', (req, res) => {
-    db.Comment.update({
-        name: req.body.name,
-        email: req.body.email,
-        message: req.body.message
-    }, {
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(updatedComment => {
-        res.json(updatedComment);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).end();
-    });
-});
-
-// DELETE /comments/:id
-router.delete('/:id', (req, res) => {
-    db.Comment.destroy({
-        where: {
-            id: req.params.id
-        }
-    })
-    .then(deletedComment => {
-        res.json(deletedComment);
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).end();
-    });
-});
 
 module.exports = router;
