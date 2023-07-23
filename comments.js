fb2803554357d06179523cc141260409a0f828ff
Shantanu Kumar <54388980+Mr-Shantanu-kumar@users.noreply.github.com>
2023-07-23 15:30:11 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,47 @@
-//create web server
-function start(){
-    console.log("Request handler 'start' was called.");
-}
+//Create web server
+const express = require('express');
+const app = express();
+
+//Create a server
+const http = require('http');
+const server = http.createServer(app);
+
+//Create socket
+const socket = require('socket.io');
+const io = socket(server);
+
+//Create a path
+const path = require('path');
+
+//Create a port
+const port = 3000;
+
+//Create a public path
+const publicPath = path.join(__dirname, './public');
+
+//Create a static path
+const staticPath = express.static(publicPath);
+
+//Use static path
+app.use(staticPath);
+
+//Create a connection
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+
+    socket.on('message', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('message', msg);
+    });
+});
+
+//Listen port
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
     
