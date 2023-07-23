32ca0edec28f9fe246f0c6d06ea23b08b036a243
Shantanu Kumar <54388980+Mr-Shantanu-kumar@users.noreply.github.com>
2023-07-23 14:59:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,5 @@
 //create web server
-function start(route, handle) {
-    function onRequest(request, response) {
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        
-        route(handle, pathname, response, request);
-    }
-    
-    http.createServer(onRequest).listen(8888);
-    console.log("Server has started.");
+function start(){
+    console.log("Request handler 'start' was called.");
 }
+    
